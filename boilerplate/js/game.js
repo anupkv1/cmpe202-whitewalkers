a71f54cafabd49e98839be4576f46860bf497269
Jasmeet Singh <https.jasmeet@gmail.com>
2017-10-28 20:49:02 -0700

    Adding functionality after understanding Player behavior to implement Observer pattern for scores

--- boilerplate/js/game.js
+++ boilerplate/js/game.js
@@ -191,5 +191,136 @@ var Baddie = me.ObjectEntity.extend({
         return true;
     }
         });
+
+var Player = me.ObjectEntity.extend({
+    init: function(x, y, settings) {
+        settings.image        = settings.image        || 'player';
+        settings.spritewidth =  40*3;
+        settings.spriteheight = 48*3;
+        settings.height = 40*3;
+        settings.width = 48*3;
+        this.parent( x, y, settings );
+        this.alwaysUpdate = true;
+        this.player = true;
+        this.hitTimer = 0;
+        this.hitVelX = 0;
+        this.image =  me.loader.getImage('player2');
+        this.necroMode = true;
+        this.overworld = false;
+
+        this.z = 200;
+        this.shootDelay = 0;
+        this.disableInputTimer = 0;
+
+        var shape = this.getShape();
+        shape.pos.x = 44;
+        shape.pos.y = 0;
+        shape.resize(70, 110);
+        me.state.current().player = this;
+
+        this.collisionTimer = 0;
+        this.deathTimer = 0;
+        this.doubleJumped = false;
+
+        this.animationSuffix = "";
+
+        this.setVelocity( 5, 16 );
+        this.setFriction( 0.4, 0 );
+        this.direction = 1;
+
+        this.centerOffsetX = 75;
+        this.centerOffsetY = 0;
+
+        this.followPos = new me.Vector2d(
+            this.pos.x + this.centerOffsetX,
+            this.pos.y + this.centerOffsetY
+        );
+
+        me.game.viewport.follow( this.followPos, me.game.viewport.AXIS.BOTH );
+        me.game.viewport.setDeadzone( me.game.viewport.width / 10, 1 );
+
+        this.renderable.animationspeed = 150;
+        this.renderable.addAnimation( "idle", [ 0, 1, 2 ] );
+        this.renderable.addAnimation( "double_jump", [ 10 , 9  ] );
+        this.renderable.addAnimation( "jump", [ 9 ] );
+        this.renderable.addAnimation( "jump_extra", [ 9 ] );
+        this.renderable.addAnimation( "fall", [ 10 ] );
+        this.renderable.addAnimation( "walk", [ 4, 5, 6, 7 ] );
+        this.renderable.addAnimation( "shoot", [ 3 ] );
+        this.renderable.addAnimation( "shoot_jump", [ 8 ] );
+        this.renderable.addAnimation( "hit", [ 11 , 11, 11] );
+
+        var offset = 12;
+        this.renderable.addAnimation( "idle_normal", [ 0 + offset, 1 + offset, 2 + offset ] );
+        this.renderable.addAnimation( "jump_normal", [ 9 + offset ] );
+        this.renderable.addAnimation( "double_jump_normal", [ 10 + offset, 9 + offset ] );
+        this.renderable.addAnimation( "jump_extra_normal", [ 9 + offset ] );
+        this.renderable.addAnimation( "fall_normal", [ 10 + offset ] );
+        this.renderable.addAnimation( "walk_normal", [ 4 + offset, 5 + offset, 6 + offset, 7 + offset ] );
+        this.renderable.addAnimation( "shoot_normal", [ 3 + offset ] );
+        this.renderable.addAnimation( "shoot_jump_normal", [ 8 + offset ] );
+        this.renderable.addAnimation( "hit_normal", [ 11 + offset , 11 + offset, 11 + offset] );
+
+        this.renderable.addAnimation( "die", [ 11 , 23, 11 , 23, 11 , 23, 11 , 23] );
+
+        this.renderable.setCurrentAnimation("idle" + this.animationSuffix);
+
+
+
+        me.input.bindKey(me.input.KEY.LEFT,  "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP,   "up", true);
+        me.input.bindKey(me.input.KEY.W,    "up", true);
+
+        me.input.bindKey(me.input.KEY.A,    "left");
+        me.input.bindKey(me.input.KEY.D,    "right");
+    },
+
+    toUnderworld: function(){
+        if(this.overworld == true) return;
+
+        this.overworld = true;
+        this.necroMode = false;
+        this.setVelocity( 7, 20 );
+        this.setFriction( 0.7, 0 );
+
+        this.disableInputTimer = 1500;
+
+        LD30.data.collectedSouls += LD30.data.souls;
+        this.renderable.animationspeed = 165;
+        if(LD30.data.souls > 0){
+            for( var i=0; i<LD30.data.souls; i++){
+                var b = new EnterPortalParticle(this.pos.x, this.pos.y, {delay:i*50});
+                me.game.world.addChild(b);
+            }
+            me.game.world.sort();
+            LD30.data.souls = 0;
+        }
+        this.animationSuffix = "_normal";
+    },
+
+    shoot: function(){
+        if(this.necroMode && this.shootDelay <= 0){
+            me.audio.play( "shoot", false, null, 0.6 );
+            var b = new Bullet(this.pos.x + 30*this.direction, this.pos.y+40, { direction: this.direction });
+            me.game.world.addChild(b);
+            me.game.world.sort();
+            this.shootDelay = 200;
+            var self = this;
+            if(this.jumping || this.falling){
+                this.renderable.setCurrentAnimation("shoot_jump" + this.animationSuffix, function() {
+                    self.renderable.setCurrentAnimation("fall" + self.animationSuffix);
+                });
+            }else{
+                this.renderable.setCurrentAnimation("shoot" + this.animationSuffix, function() {
+                    self.renderable.setCurrentAnimation("idle" + self.animationSuffix);
+                })
+            }
+
+
+        }
+    },
+})
+
    
 
