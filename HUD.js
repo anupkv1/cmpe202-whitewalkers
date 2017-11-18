dcd9d660a6110b4171577abccaacd95fab5cf181
Parag Vijayvergia <parag.vijayvergia@sjsu.edu>
2017-11-17 22:29:24 -0800

    Implementing Heads Up display for scores

---
+++ HUD.js
@@ -0,0 +1,67 @@
+/**
+ * a HUD container and child items
+ * Heads Up Display AKA Status Bar
+ */
+
+game.HUD = game.HUD || {};
+
+
+game.HUD.Container = me.Container.extend({
+
+    init: function() {
+        // call the constructor
+        this._super(me.Container, 'init');
+
+        // persistent across level change
+        this.isPersistent = true;
+
+        // make sure we use screen coordinates
+        this.floating = true;
+
+        // give a name
+        this.name = "HUD";
+
+        // add our child score object at the top left corner
+        this.addChild(new game.HUD.ScoreItem(-10, -10));
+    }
+});
+
+
+/**
+ * a basic HUD item to display score
+ */
+game.HUD.ScoreItem = me.Renderable.extend({
+    /**
+     * constructor
+     */
+    init: function(x, y) {
+
+        // call the parent constructor
+        // (size does not matter here)
+        this._super(me.Renderable, 'init', [x, y, 10, 10]);
+
+        // local copy of the global score
+        this.score = -1;
+    },
+
+    /**
+     * update function
+     */
+    update : function (dt) {
+        // we don't do anything fancy here, so just
+        // return true if the score has been updated
+        if (this.score !== game.data.score) {
+            this.score = game.data.score;
+            return true;
+        }
+        return false;
+    },
+
+    /**
+     * draw the score
+     */
+    draw : function (context) {
+        // draw it baby !
+    }
+
+});
